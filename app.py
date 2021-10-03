575312fa20f768033366596ab8aad07de62ffd8b
Mao <amcaicedo@unimayor.edu.co>
2021-10-03 11:35:56 -0500

    Prueba de proyecto con framework python Flask y frontend con materializecss

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        #print(f'{username}, {password}')
+        print('{}, {}'.format(username, password))
+        return 'Datos enviados'
